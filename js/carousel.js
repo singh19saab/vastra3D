25c15c8299d263154cf6fec245175e38c6d8b80f
Abhishek Kumar <kumar.1905abhishek@gmail.com>
2020-12-01 19:01:48 +0530

    Update on 1/12/2020

---
+++ js/carousel.js
@@ -0,0 +1,11 @@
+
+$('.autoplay').slick({
+    adaptiveHeight:true,
+    focusOnSelect:true,
+    swipeToSlide:true,
+    lazyLoad:'ondemand',
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+});
